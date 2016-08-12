96259cdb5b384126d755ae8955368dbf922b11e3
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2016-08-12 10:08:34 +0200

    First import

---
+++ js/redrawDoll.js
@@ -0,0 +1,34 @@
+/*global app*/
+
+app.ports.redrawDoll.subscribe(function(evt) {
+    var canvas = document.getElementById(evt.dollCanvasId),
+        canvasContext = canvas.getContext("2d"),
+        multiplierX = evt.wardrobe.dollWidth + evt.wardrobe.spacerWidth,
+        multiplierY = evt.wardrobe.dollHeight + evt.wardrobe.spacerHeight;
+
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    var selectionDict = {};
+    evt.outfitSelections.forEach(function(part) {
+        selectionDict[part[0]] = part[1];
+    });
+
+    evt.wardrobe.drawers.forEach(function(drawer) {
+        if (selectionDict.hasOwnProperty(drawer.id)) {
+            var img = document.getElementById(drawer.id),
+                drawerCoords = selectionDict[drawer.id];
+
+            canvasContext.drawImage(img,
+                                    multiplierX * drawerCoords.drawerCol,
+                                    multiplierY * drawerCoords.drawerRow,
+                                    evt.wardrobe.dollWidth,
+                                    evt.wardrobe.dollHeight,
+                                    0,
+                                    0,
+                                    evt.wardrobe.dollWidth,
+                                    evt.wardrobe.dollHeight);
+        }
+    });
+
+    app.ports.pngExportResponder.send(canvas.toDataURL());
+});
